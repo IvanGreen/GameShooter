f4d7ed031d9e630b64989c28b12b68260d59dce8
IvanGreen <mylinkou@bk.ru>
2019-04-10 20:25:30 +0300

    На половину сделал

--- core/src/com/greencode/game/screen/MenuScreen.java
+++ core/src/com/greencode/game/screen/MenuScreen.java
@@ -11,13 +11,16 @@ public class MenuScreen extends BaseScreen{
     private Vector2 touch;
     private Vector2 pos;
     private Vector2 v;
+    private Vector2 end;
+    private final float time = 60; //60 кадров в секунду
 
     @Override
     public void show() {
         super.show();
         touch = new Vector2();
-        v = new Vector2();
+        v = new Vector2(0,0);
         pos = new Vector2();
+        end = new Vector2();
         img = new Texture("C:\\Users\\Ivan Green\\Desktop\\Geek Brains\\AndroidDevelop\\GameShooter\\android\\assets\\badlogic.jpg");
 
         //если пишу короткий путь, то выдает ошибку, хз почему, просьба помочь.
@@ -27,8 +30,12 @@ public class MenuScreen extends BaseScreen{
     public void render(float delta) {
         super.render(delta);
 		batch.begin();
+		pos.add(v);
 		batch.draw(img, pos.x,pos.y);
 		batch.end();
+		if (pos.y > end.y && pos.x > end.x){
+		    v.setZero();
+        }
     }
 
     @Override
@@ -44,21 +51,21 @@ public class MenuScreen extends BaseScreen{
         /**
          * Как я себе это представлял, но это не работает
          */
-//
-//        float s1 = screenX - pos.x; //определяем сколько точек надо пройти по оси Х
-//        float s2 = (Gdx.graphics.getHeight() - screenY) - pos.y; //определяем сколько точек надо пройти по оси У
-//
-//        v = new Vector2(s1/60,s2/60); //делим расстояние на 60 кадров и получаем скорость
-//
-//        do{
-//            pos.add(v); //делаем пока
-//        } while (pos.x != screenX && pos.y != screenY); //координаты не совпадут с местом назначения
+
+            end.x = screenX;
+            end.y = Gdx.graphics.getHeight() - screenY;
+
+            float s1 = screenX - pos.x; //определяем сколько точек надо пройти по оси Х
+            float s2 = (Gdx.graphics.getHeight() - screenY) - pos.y; //определяем сколько точек надо пройти по оси У
+
+            v = new Vector2(s1 / time, s2 / time); //делим расстояние на 60 кадров и получаем скорость
+
 
         /**
          * А дальше рабочий, но не тот метод.
          */
-        pos.x = screenX;
-        pos.y = Gdx.graphics.getHeight() - screenY;
+//        pos.x = screenX;
+//        pos.y = Gdx.graphics.getHeight() - screenY;
 
         //реализовал "моментальное перемещение", а вот как анимировать все это дело - я хз (о_О)
 
