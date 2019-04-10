1d44fbd985b0be654de84516a2f26f229508c6cc
IvanGreen <mylinkou@bk.ru>
2019-04-10 16:52:31 +0300

    Вообще повеситься можно

--- core/src/com/greencode/game/base/BaseScreen.java
+++ core/src/com/greencode/game/base/BaseScreen.java
@@ -1,17 +1,19 @@
 package com.greencode.game.base;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public abstract class BaseScreen implements Screen {
+public abstract class BaseScreen implements Screen, InputProcessor {
 
     protected SpriteBatch batch;
 
     @Override
     public void show() {
         System.out.println("show");
+        Gdx.input.setInputProcessor(this);
         batch = new SpriteBatch();
     }
 
@@ -47,5 +49,52 @@ public abstract class BaseScreen implements Screen {
     public void dispose() {
         System.out.println("dispose");
         batch.dispose();
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        System.out.println("KeyDown keycode = " + keycode);
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        System.out.println("KeyUp keycode = " + keycode);
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        System.out.println("KeyTyped character = " + character);
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        System.out.println("TouchDown screenX = " + screenX + " screenY = " + screenY);
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        System.out.println("TouchUp screenX = " + screenX + " screenY = " + screenY);
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        System.out.println("TouchDragged screenX = " + screenX + " screenY = " + screenY);
+
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
     }
 }
--- core/src/com/greencode/game/screen/MenuScreen.java
+++ core/src/com/greencode/game/screen/MenuScreen.java
@@ -1,23 +1,33 @@
 package com.greencode.game.screen;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
 import com.greencode.game.base.BaseScreen;
 
-public class MenuScreen extends BaseScreen {
+public class MenuScreen extends BaseScreen{
 
     private Texture img;
+    private Vector2 touch;
+    private Vector2 pos;
+    private Vector2 v;
 
     @Override
     public void show() {
         super.show();
-        img = new Texture("badlogic.jpg");
+        touch = new Vector2();
+        v = new Vector2();
+        pos = new Vector2();
+        img = new Texture("C:\\Users\\Ivan Green\\Desktop\\Geek Brains\\AndroidDevelop\\GameShooter\\android\\assets\\badlogic.jpg");
+
+        //если пишу короткий путь, то выдает ошибку, хз почему, просьба помочь.
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, pos.x,pos.y);
 		batch.end();
     }
 
@@ -25,5 +35,34 @@ public class MenuScreen extends BaseScreen {
     public void dispose() {
         super.dispose();
         img.dispose();
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        touch.set(screenX, Gdx.graphics.getHeight() - screenY);
+
+        /**
+         * Как я себе это представлял, но это не работает
+         */
+//
+//        float s1 = screenX - pos.x; //определяем сколько точек надо пройти по оси Х
+//        float s2 = (Gdx.graphics.getHeight() - screenY) - pos.y; //определяем сколько точек надо пройти по оси У
+//
+//        v = new Vector2(s1/60,s2/60); //делим расстояние на 60 кадров и получаем скорость
+//
+//        do{
+//            pos.add(v); //делаем пока
+//        } while (pos.x != screenX && pos.y != screenY); //координаты не совпадут с местом назначения
+
+        /**
+         * А дальше рабочий, но не тот метод.
+         */
+        pos.x = screenX;
+        pos.y = Gdx.graphics.getHeight() - screenY;
+
+        //реализовал "моментальное перемещение", а вот как анимировать все это дело - я хз (о_О)
+
+        System.out.println("TouchDown screenX = " + touch.x + " screenY = " + touch.y);
+        return false;
     }
 }
